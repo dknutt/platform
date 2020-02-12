ff4008ddbec63882557cfbd645fd5eec0717eb03
Coach Dee <47064845+dknutt@users.noreply.github.com>
2020-02-12 18:02:21 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,50 +1,38 @@
 
-let hero = new Hero() //instantiate and store new Hero in hero variable
-let shapes = []
-ERASE()
-hero.draw()
+let hero = new Hero()
+let keyPressed = {}
 
+// setup a function to run whenever a key is pressed
 window.addEventListener("keydown", event => {
-	console.log (event.code)
-	if (event.code === "KeyS"){
-		let colorIndex = Math.random()
-		let color = "red"
-		if (colorIndex < 0.5){
-			color = "blue"
-		}
-		if (colorIndex > 0.6){
-			color = "orange"
-		}
+	keyPressed[event.code] = true
 
-		CTX.fillStyle = color
-
-		let x = Math.random() * CANVAS.width
-		let y = Math.random() * CANVAS.height
-
-		let width = Math. random() * CANVAS.width
-		let height = Math.random() * CANVAS.height
-
-		let s = new Shape(x,y,width,height)
-		shapes.push(s)
-	}
-	if (event.code === "ArrowRight"){
-		hero.moveRight()
-	}
-	ERASE()
-
-	//console.log (shapes)
-	shapes.forEach(s => s.draw())
-
-	hero.draw()
 })
 
+window.addEventListener("keyup", event => {
+	keyPressed[event.code] = false
+})
 
+function loop() {
+	console.log("sometimes I hide things in plain sight")
+	if (keyPressed["ArrowUp"]){
+		hero.moveUp()
+	}
+	if (keyPressed["ArrowDown"]){
+		hero.moveDown()
+	}
 
-/*
-Stretch Goals:
-1. x & y should cover screen
-	change 500 in line 10 to canswas.width and line 11 to canvas.height
-2. support 3 random colors
-3. random width and height
+	;if (keyPressed["ArrowLeft"]){
+		hero.moveLeft()
+	}
+	if (keyPressed["ArrowRight"]){
+		hero.moveRight()
+	}
 
-*/
+	//draw everything
+	ERASE()
+	hero.draw()
+
+	setTimeout(loop, 1000 / 60)
+}
+loop()
+
--- hero.js
+++ hero.js
@@ -1,13 +1,25 @@
+let heroSprite = new Sprite("jumpman.png")
+
 class Hero {
 	constructor(){ // how you intialize everything
 		this.x = 100
 		this.y = 100
 		this.width = 100
+		this.height = 100
 	}
-
+	moveUp(){ // helper function
+		this.y = this.y - 10
+	}
+	moveDown(){ // helper function
+		this.y = this.y + 10
+	}
+	moveLeft(){ // helper function
+		this.x = this.x - 10
+	}
 	moveRight(){ // helper function
 		this.x = this.x + 10
 	}
+
 	draw(){
 		CTX.fillStyle = "black"
 		CTX.beginPath()
@@ -19,5 +31,7 @@ class Hero {
 			2* Math.PI,// endpoint
 			)
 		CTX.fill()
+
+		heroSprite.draw(this.x, this.y, this.width, this.height)
 	}
 }
